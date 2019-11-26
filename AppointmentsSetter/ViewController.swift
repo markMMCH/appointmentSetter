80784afa4156942ba983e579865de950c34da9f0
MarkMMCH <lyubik.chibukhchian@gmail.com>
2019-11-26 10:53:05 +0400

    Initial Commit

---
+++ AppointmentsSetter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppointmentsSetter
+//
+//  Created by Mark Chibukhchian on 11/26/19.
+//  Copyright © 2019 Mark Chibukhchian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
